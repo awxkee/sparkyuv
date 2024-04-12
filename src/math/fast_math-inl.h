4c06557a577bd79312b0d34e34de5b6486351ddf
awxkee <radzivon.bartoshyk@proton.me>
2024-04-12 20:38:25 +0100

    Adding Windows build

--- src/math/fast_math-inl.h
+++ src/math/fast_math-inl.h
@@ -33,6 +33,10 @@
 #endif
 #include <cmath>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #if defined(__GNUC__) || defined(__clang__)
 #define HWY_FAST_MATH_INLINE inline __attribute__((flatten))
 #else
