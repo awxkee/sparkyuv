6033cb3366fae74ada3945141881d8ed19dbe527
awxkee <radzivon.bartoshyk@proton.me>
2024-04-12 15:48:36 +0100

    Added android autobuild

---
+++ ci_android_runner.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+ABI_LIST="armeabi-v7a arm64-v8a x86 x86_64"
+
+for abi in ${ABI_LIST}; do
+  rm -rf "build-${abi}"
+  mkdir "build-${abi}"
+  cd "build-${abi}"
+
+  cmake .. \
+    -G "Ninja" \
+    -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=${abi} -DCMAKE_ANDROID_ARCH_ABI=${abi} \
+    -DANDROID_NDK=${ANDROID_NDK_HOME} \
+    -DSJPEG_ANDROID_NDK_PATH=${ANDROID_NDK_HOME} \
+    -DANDROID_PLATFORM=android-21 \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED=OFF \
+    -DCMAKE_SYSTEM_NAME=Generic \
+    -DCMAKE_ANDROID_STL_TYPE=c++_shared \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DCMAKE_THREAD_PREFER_PTHREAD=TRUE \
+    -DANDROID=TRUE
+  ninja
+  cd ..
+done
