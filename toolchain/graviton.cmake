115be1924952c23d6518d5b48e300208e2d693c2
awxkee <radzivon.bartoshyk@proton.me>
2024-04-12 17:24:37 +0100

    Adding graviton build

--- toolchain/graviton.cmake
+++ toolchain/graviton.cmake
@@ -1,2 +1,4 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mcpu=neoverse-n1 -march=armv8.2-a")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mcpu=neoverse-n1 -march=armv8.2-a")
