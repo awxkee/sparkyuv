3f879b63b35af7672aa9c4b7e2f6e7de09ae00b7
awxkee <radzivon.bartoshyk@proton.me>
2024-04-12 17:09:36 +0100

    Adding graviton build

--- toolchain/ci_android_runner.sh
+++ toolchain/ci_android_runner.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# Copyright (C) 2024 Radzivon Bartoshyk
+#
+# This file belongs to sparkyuv project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 set -e
 
 ABI_LIST="armeabi-v7a arm64-v8a x86 x86_64"
@@ -19,6 +37,6 @@ for abi in ${ABI_LIST}; do
     -DCMAKE_ANDROID_STL_TYPE=c++_shared \
     -DCMAKE_SYSTEM_NAME=Android \
     -DANDROID=TRUE
-  ninja
+  ninja -j 8
   cd ..
 done
