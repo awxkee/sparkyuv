2cbde89019e22306cbfc88c124172d2c0a82e993
awxkee <radzivon.bartoshyk@proton.me>
2024-04-19 17:15:27 +0100

    Added gaussian blur, some refactor

--- src/FastGaussian.cpp
+++ src/FastGaussian.cpp
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "sparkyuv-def.h"
 #include "sparkyuv-internal.h"
 #include "PixelLoader.h"
 #include "concurrency.hpp"
